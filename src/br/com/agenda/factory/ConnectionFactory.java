e9c16871b6b81fcbe0b04dc382e60217acf7f8b1
Renato Andrade <dev.renatopandrade@gmail.com>
2022-05-24 09:22:41 -0300

    database connection done

--- src/br/com/agenda/factory/ConnectionFactory.java
+++ src/br/com/agenda/factory/ConnectionFactory.java
@@ -2,9 +2,29 @@ package br.com.agenda.factory;
 
 // TODO: encapsulate secrets in environment variables
 
+import com.mysql.jdbc.Connection;
+import java.sql.DriverManager;
+
 public class ConnectionFactory {
     private static final String USERNAME="root";
-    private static final String PASSWORD="root";
+    private static final String PASSWORD="password";
+    private static final String DATABASE_URL= "jdbc:mysql://localhost:3306/agenda";
 
+    public static Connection createConnectionToMySQL() throws Exception {
+        // Loads class in JVM
+        Class.forName("com.mysql.jdbc.Driver");
 
+        Connection connection = (Connection) DriverManager.getConnection(DATABASE_URL, USERNAME, PASSWORD);
+
+        return connection;
+    }
+
+    public static void main(String[] args) throws Exception {
+        Connection con = createConnectionToMySQL();
+
+        if(con != null){
+            System.out.printf("Conexão com sucesso!");
+            con.close();
+        }
+    }
 }
