1f961b4ab2eb6a9789c0e6eecfbf14133995392e
Renato Andrade <dev.renatopandrade@gmail.com>
2022-05-24 08:50:16 -0300

    initial commit

---
+++ src/br/com/agenda/factory/ConnectionFactory.java
@@ -0,0 +1,10 @@
+package br.com.agenda.factory;
+
+// TODO: encapsulate secrets in environment variables
+
+public class ConnectionFactory {
+    private static final String USERNAME="root";
+    private static final String PASSWORD="root";
+
+
+}
